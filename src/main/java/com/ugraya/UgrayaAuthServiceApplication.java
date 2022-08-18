2a5996de13c4ed4ba954d525198c107609dc0d58
amoldeole <amoldeole511@gmail.com>
2022-08-19 02:00:17 +0530

    Ugraya-1: First commit for the ugraya-auth-service with basic implementation

---
+++ src/main/java/com/ugraya/UgrayaAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ugraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgrayaAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgrayaAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ugraya/entities/UserDao.java
@@ -0,0 +1,43 @@
+package com.ugraya.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserDao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String username;
+    @Column
+    @JsonIgnore
+    private String password;
+    @Column
+    private String email;
+    @Column
+    private String enabled;
+    @Column
+    private String accountNonExpired;
+    @Column
+    private String credentialsNonExpired;
+    @Column
+    private String accountNonLocked;
+
+}
